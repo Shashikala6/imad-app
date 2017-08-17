b288645fb94a5c45865895e8c55f8cf482d3f3cd
Shashikala6 <shashi.sannu6@gmail.com>
2017-08-17 17:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artcle-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article-one', function(req, res){
-    res.send('Article-one and responded here');
+     res.sendFile(path.join(__dirname, 'ui', 'artcle-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
