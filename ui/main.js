e595b1995d6bfbe6bda45a87cac7a3f2493e44fa
Shashikala6 <shashi.sannu6@gmail.com>
2017-08-22 20:27:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 // change the text of new value
 var element = document.getElementById('main-text');
 element.innerHTML =' NEW VVALUE';
+// move the image
+var img = document.getElementById('img');
+img.onclick = function(_) {
+    img.style.margineLeft='100px';
+};
